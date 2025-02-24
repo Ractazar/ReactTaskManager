ac8b95b28a52fd9e5ae96ca94c8a5f925c793b6b
Ractazar <67387285+Ractazar@users.noreply.github.com>
2025-02-24 10:50:52 -0300

    Delete components directory

--- components/Search.jsx
+++
@@ -1,8 +0,0 @@
-const Search = ({search, setSearch}) => {
-  return <div className="search">
-    <h2>Pesquisar Título:</h2>
-    <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder = "Digite para pesquisar" />
-  </div>;
-};
-
-export default Search
